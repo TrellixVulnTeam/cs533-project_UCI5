edd4691f5a33a942281a6faff871e11b18fef44b
yuxuanc5 <yuxuanc5@illinois.edu>
2022-05-02 01:08:59 -0500

    executable

--- executable/attacker.c
+++ executable/attacker.c
@@ -3,32 +3,40 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define TRIALS 1000
+
+int64_t timespec_diffns(struct timespec * a, struct timespec* b) {
+    return (a->tv_nsec - b->tv_nsec) % 1000000000;
+}
 
 int main() {
     int64_t t0, t1;
+    struct timespec ts0, ts1;
+    // struct timespec ts[TRIALS + 1];
+    int64_t stat[TRIALS] = {};
+    struct timespec slp_duration = {.tv_sec = 0, .tv_nsec=1};
+    char *func = (char*)bignum_imultiply;
+    char temp;
 
-    bignum *base = bignum_init();
-    bignum *pow = bignum_init();
-    bignum *mod = bignum_init();
-    bignum *result = bignum_init();
+    for (int i = 0; i < TRIALS; ++i) {
+        __asm__ volatile("clflush (%0)" ::"r"(((char *)&bignum_imultiply) + 0));
+        // __asm__ volatile("clflush (%0)" ::"r"(((char *)&bignum_imultiply) + 128));
+        // t0 = _rdtsc();
+        clock_gettime(CLOCK_REALTIME, &ts0);
+        sleep(1);
+        // temp = *func;
+        clock_gettime(CLOCK_REALTIME, &ts1);
+        // t1 = _rdtsc();
+        stat[i] = timespec_diffns(&ts1, &ts0);
+        // stat[i] = t1 - t0;
+    }
 
-    bignum_fromstring(base, "2");
-    bignum_fromstring(pow, "27");
-    bignum_fromstring(mod, "65537");
-    t0 = _rdtsc();
-    bignum_modpow(base, pow, mod, result);
-    t1 = _rdtsc();
-
-    bignum_print(result);
-    printf("\n");
-
-    bignum_deinit(base);
-    bignum_deinit(pow);
-    bignum_deinit(mod);
-    bignum_deinit(result);
-
-
-    printf("t0 %ld t1 %ld\n", t0, t1);
-    printf("elapsed %ld\n", t1 - t0);
+    printf("stat\n");
+    for (int i = 0; i < TRIALS; ++i) {
+        printf("%d:\t %ld\n", i, stat[i]);
+    }
     return 0;
 }
